e64302f794113ec1e6d367ffda7ab8cc158447db
Himanshihub <78586080+Himanshihub@users.noreply.github.com>
2022-03-12 10:47:13 +0530

    Add files via upload

---
+++ SourceCode/conn.java
@@ -0,0 +1,20 @@
+package airline.management.system;
+
+import java.sql.*;  
+
+public class conn{
+    Connection c;
+    Statement s;
+    public conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/AirlineDatabase","root","");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
